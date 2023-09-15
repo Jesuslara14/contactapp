e53e3a57405cc227e29e0b4b941289978fb613b4
Jesuslara14 <111470200+Jesuslara14@users.noreply.github.com>
2023-09-15 12:16:56 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,33 @@
+## Contacts application
+
+This app allows you to locally host an application which allows the user to store as many contacts as they want with a Name and Email.
+
+>### Dependencies
+>
+> - Express
+> - Body-Parser
+> - Ejs
+> - Nodemon (Optional)
+
+> ### Install dependencies with the following command
+>
+>>npm i express body-parser ejs nodemon
+
+### File structure
+
+Arrange the files in the following structure
+
+<b>
+
+- Root Folder
+    - data
+        - contacts.json
+    - views
+        - partials
+            - nav.ejs
+        - add.ejs
+        - edit.ejs
+        - index.ejs
+        - view.ejs
+    - app.js
+     
---
+++ promiseapp.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs').promises;
+
+const app = express();
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+async function readContactsFile(){
+    const data = await fs.readFile('./data/contacts.json', 'utf-8');
+    return JSON.parse(data);
+}
+
+async function writeContactsFile(contacts){
+    await fs.writeFile('./data/contacts.json', JSON.stringify(contacts));
+}
+
+async function findContact(id){
+    const contacts = await readContactsFile();
+    const contact = await contacts.find(contact => contact.id === id);
+    const index = contacts.indexOf(contact);
+    return {
+        contact: contact,
+        index: index
+    }
+}
+
+app.get('/', async (req, res) => {
+    try{
+        const contacts = await readContactsFile();
+        res.render('index', {contacts});
+    }catch(error){
+        console.error(error)
+        res.status(500).send('Internal Server Error')
+    }
+});
+
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+app.post('/add', async (req, res) => {
+    try{
+        const newContact = {
+            name: req.body.name, 
+            email: req.body.email, 
+            id: Date.now()
+        };
+        const contacts = await readContactsFile();
+        contacts.push(newContact);
+        await writeContactsFile(contacts);
+        res.redirect('/');
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/edit/:id', async (req, res) => {
+    try{
+        const contactFound = await findContact(parseInt(req.params.id));
+        const contact = contactFound.contact;
+        res.render('edit', {contact});
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.post('/edit/:id', async (req, res) => {
+    try{
+        const contact = await findContact(parseInt(req.params.id));
+        const contacts = await readContactsFile();
+        contacts[contact.index] = {
+            name: req.body.name, 
+            email: req.body.email, 
+            id: req.params.id
+        };
+        await writeContactsFile(contacts);
+        res.redirect('/');
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/delete/:id', async (req, res) => {
+    try{
+        const contact = await findContact(parseInt(req.params.id));
+        const contacts = await readContactsFile();
+        contacts.splice(contact.index, 1);
+        await writeContactsFile(contacts);
+        res.redirect('/');
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/view/:id', async (req, res) => {
+    try{
+        const contactFound = await findContact(parseInt(req.params.id));
+        const contact = contactFound.contact;
+        res.render('view', {contact});
+    }catch(error){
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server now running at ${PORT}`);
+});
